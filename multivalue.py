360db09601d2ce8d8682c661094772c9cc60fcf8
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-01 21:57:50 +0700

    Update multivalue.py

--- multivalue.py
+++ multivalue.py
@@ -1,3 +1,15 @@
-#used for multiple value and variable
+#used for multiple value and variable, use commas symbol
+a, b, c = "kopi", "es krim", "wafer"
+print(a)
+#many value mulitple variables, u can use equal symbol in variable
+x = y = z = "mie ayam"
+print(x)
+r = s = t = 2
+print(r)
 
-#many value mulitple variables
+#unpacking which it is used for collection some value in a list and you want extract some values to variables
+mieAyam = ["mie", "ayam", "kecap", "saus", "air minum"]
+j, k, l, m, n = mieAyam
+
+print(j)
+#if you unpacking make sure value and variables have same value, unpacking is basic before learning tuples

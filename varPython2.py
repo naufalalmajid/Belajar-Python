9897dae4d63ca3ff9fceb19be821298e0e93c906
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-01 20:41:54 +0700

    Create varPython2.py

---
+++ varPython2.py
@@ -0,0 +1,11 @@
+#Camel Case which each word exect first use capital letter
+justVariable = 5
+#Pascal Case which each word use capital letter
+JustVariable = 5
+#Snake Case which each word separate by underscore
+just_variable v = 5
+
+"""
+variable is case sensitive
+illegal variable is using number in first, space and hyphen
+"""

49c0f80854da6be5a6664ccdc686c6af21e11b68
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-01 19:42:06 +0700

    Update varPython.py

--- varPython.py
+++ varPython.py
@@ -1,1 +1,13 @@
+#comment in line
+"""
+comment multiple line
+"""
 
+x = 3                 #it is integer variable
+y = "we build dream"  #it is string variable
+
+#we can declare variable like this
+
+a = str(5)            #it is 5 but as string variable
+b = int(5)            #it is 5 but as integer variable
+c = float(9)          #it is 9 but as float or decimal variable 

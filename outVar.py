b2f08860a3b7bbf9fa2b7afdaa81e7604db5609d
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-02 23:55:35 +0700

    Update outVar.py

--- outVar.py
+++ outVar.py
@@ -2,8 +2,11 @@
 x = "alma rajin belajar"
 #from variable x we want output x use this command
 print(x)
-#if multiple variable we do like this
+#if multiple variable we do like this, separate with commas will print with space
 a = "alma"
 b = "rajin"
 c = "belajar"
 print(a, b, c)
+#or we can use + symbol, but it is will print without space (it is can used as mathematical operator)
+print(a + b + c)
+#remember to type value variable, different variable may will error

92404a86347914b96970718eb09f1c65bb81c14a
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-02 23:50:56 +0700

    Update outVar.py

--- outVar.py
+++ outVar.py
@@ -1,4 +1,9 @@
 #some command used for output value is print
-x = "alma rajin belajar
+x = "alma rajin belajar"
 #from variable x we want output x use this command
 print(x)
+#if multiple variable we do like this
+a = "alma"
+b = "rajin"
+c = "belajar"
+print(a, b, c)

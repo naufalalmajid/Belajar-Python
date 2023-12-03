05d4190be70557240aec8a5598171fe29f3c4810
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-03 22:21:54 +0700

    Update globalVar.py

--- globalVar.py
+++ globalVar.py
@@ -1,14 +1,30 @@
-#it is some variable which can used both inside and outside function
+# it is some variable which can used both inside and outside function
 # it is some variable which can used both inside and outside function
 # outside variable
 x = "alma "
 
-def myFunc():
+
+def myfunc():
     # inside variable
     x = "naufal"
     # inside variable call
     print(x + "rajin belajar")
 
-myFunc()
+
+myfunc()
 # outside variable call
 print(x + "rajin belajar")
+
+# it is will replace with global variable
+a = "al majid"
+# global variable
+
+def myfunc2():
+    # global variable will make a variable can call everywhere
+    global a
+    a = "naufal al majid "
+    print(a + "rajin belajar")
+
+
+myfunc2()
+print(a + "rajin belajar")

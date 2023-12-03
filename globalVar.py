ea2230bfec679ed43dd35e1db990f5445fc84deb
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-03 22:12:11 +0700

    Update globalVar.py

--- globalVar.py
+++ globalVar.py
@@ -1,6 +1,14 @@
+#it is some variable which can used both inside and outside function
+# it is some variable which can used both inside and outside function
+# outside variable
 x = "alma "
 
 def myFunc():
+    # inside variable
+    x = "naufal"
+    # inside variable call
     print(x + "rajin belajar")
 
 myFunc()
+# outside variable call
+print(x + "rajin belajar")

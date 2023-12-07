979ac9d55c84e3d2ac5278bcfe737a7843d92e10
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-07 22:33:31 +0700

    Update numPython.py

--- numPython.py
+++ numPython.py
@@ -1,1 +1,13 @@
+#from datatypes.txt we will learning about python numbers
+#int (int, as integer)
+x = 2
 
+#float (float, as decimal)
+y = 2.222
+
+#complex (complex, as complex value)
+z = 5 + 6j
+
+print(type(x))
+print(type(y))
+print(type(z))

ce87d4f1660521029aa34ac4f08d9ca38fc5c4c8
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-08 08:07:01 +0700

    Update numPython2.py

--- numPython2.py
+++ numPython2.py
@@ -1,1 +1,27 @@
+# integer, unlimited positive or negative without decimal
+x = 222222222222222222
+y = -99999999999999999
+z = 24
 
+print(type(x))
+print(type(y))
+print(type(z))
+
+# float, unlimited values positive or negative but decimal
+a = 24.0
+b = 2222222222.2222222
+c = -222222222.2222222
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# complex, for any complex values
+
+j = 5j
+k = 3 + 6j
+l = 5 + j
+
+print(type(j))
+print(type(k))
+print(type(l))

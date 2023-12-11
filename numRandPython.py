42948e70d50aa6f70dab10d3b3f89b68aa5ebfed
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-11 22:27:00 +0700

    Update numRandPython.py

--- numRandPython.py
+++ numRandPython.py
@@ -1,1 +1,7 @@
+# for first we need random module
+import random
 
+# it is just simple random print some value from 2 to 8 (9 is excluded here)
+print(random.randrange(2, 9))
+
+# for more we need learning on random module documentation

557b8a98f2d9a69c75bfc23d09e109d5490e3ca8
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-11 21:48:29 +0700

    Update numberConv.py

--- numberConv.py
+++ numberConv.py
@@ -1,1 +1,25 @@
+# in some case maybe we want convert results calculation likes float number to decimal so we will learning in here
+
+x = 1
+y = 2.8
+
+print(int(x * y))
+print(float(x * y))
+print(complex(x * y))
+
+# or we want convert instantly to another type number
+
+a = 2.8
+b = 7
+
+# convert
+
+j = int(a)
+k = float(b)
+l = complex(a)
+
+print(type(j))
+print(type(k))
+print(l, type(l))
+# for some notes if we want convert complex number to another types we may need more codes
 

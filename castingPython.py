aa29edcf06dd2ed47e1dff6a48912460674badd5
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-13 01:00:50 +0700

    Update castingPython.py
    sorry for late commit

--- castingPython.py
+++ castingPython.py
@@ -1,1 +1,28 @@
+# casting is method what used for make specific data on variable
 
+# for integer value
+x = int(2.8)
+y = int(28)
+z = int("5")
+
+print(type(x), x)
+print(type(y), y)
+print(type(z), z)
+
+# for float value
+a = float(2.8)
+b = float(28)
+c = float("5.55")
+
+print(type(a), a)
+print(type(b), b)
+print(type(c), c)
+
+# for string type
+j = str(2.8)
+k = str(28)
+l = str("2 for luck")
+
+print(type(j), j)
+print(type(k), k)
+print(type(l), l)

ac58a88f4c47ebf86bf52720930dfca5e0e40823
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-14 11:02:07 +0700

    Update strings2.py

--- strings2.py
+++ strings2.py
@@ -1,1 +1,13 @@
-x
+# in this file we will more learning about strings
+
+# string are array, we can use square brackets for access element of string(with value count from 0)
+x = "alma still learning"
+print(x[2])
+
+# now we can loop use for loops
+for x in "al majid":
+    print(x)
+
+# we can count length use len() function
+y = "building the dream"
+print(len(y))

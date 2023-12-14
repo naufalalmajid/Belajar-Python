b4da57176c96ac3fb804834b1a65402a09ea8336
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-14 10:27:23 +0700

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,1 +1,25 @@
+# strings are some collection of word or other character
+# strings in python was surrounded by either single or double quotation marks
 
+# string only
+print('learner')
+print("worker")
+
+# a string to variable
+x = 'alma still learning'
+y = "alma builds his dream"
+
+print(x)
+print(y)
+
+# multiline string, we can use three quotation marks 
+z = '''life
+is
+about
+grow'''
+print(z)
+print(
+    """let's 
+do 
+it"""
+)

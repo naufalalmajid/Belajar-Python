16793fc3e5d01216aa1de4f0bde726e4e080ff10
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-15 20:26:48 +0700

    Update strings3.py

--- strings3.py
+++ strings3.py
@@ -1,1 +1,19 @@
+# now we can check string
+print("alma always learning everyday")
 
+x = "alma always learning everyday"
+print("learning" in x)
+
+# using if in string
+x = "alma always learning everyday"
+if "learning" in x:
+    print("Yes, 'learning' available")
+
+# now we can try use if not
+x = "alma always learning everyday"
+print("tired" not in x)
+
+# using if not
+x = "alma always learning everyday"
+if "tired" not in x:
+    print("yes, nothing word 'tired' for learning")

2c50ac65dddaef63caf8656fb830380f6c1b898c
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-17 20:56:00 +0700

    Update stringsModify.py

--- stringsModify.py
+++ stringsModify.py
@@ -1,1 +1,22 @@
+# we can modify strings with some methods
 
+# make upper case from strings
+x = "hai"
+print(x.upper())
+
+# or make lower case
+y = "INI ALMA, KAMU MENGINGATNYA?"
+print(y.lower())
+
+# removed whitespace
+z = " kamu lupa?? "
+print(z.strip())
+
+# or replace strings
+a = "padahal kita tidak mengenal dulunya"
+print(a.replace("tidak", "saling"))
+
+b = "iya mungkin waktu terlalu cepat berlalu"
+print(b)
+
+# it is too much for write here so we can read list of strings methods in stringsMethods.txt

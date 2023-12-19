892f6867a1c3a700d34f8c10431cfb1716557ebf
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-19 16:47:22 +0700

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,16 @@
+# lists is used for store multiple items in single variable, with using square brackets
+
+x = ["alma", "always", "learning", "everything", "new", "day", "by", "day"]
+print(x)
+
+# list are ordered, changeable, allow duplicate values
+# we can count how much items in lists
+print(len(x))
+
+# with list we can stored any type of data like integer, boolean, and strings
+# list can check with type()
+y = ["sudah", "lama", "hampir", 24, "jam", True]
+print(type(y))
+
+thislist = list(("apple", "banana", "cherry"))  # note the double round-brackets
+print(thislist)

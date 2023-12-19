cb1226cee821786f942771a06949c9569adb2dc5
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-19 13:47:04 +0700

    Update booleans.py

--- booleans.py
+++ booleans.py
@@ -1,1 +1,14 @@
+# booleans just given True or False result
+# we can use boolean for evaluate some expression
 
+print(28 > 2)
+print(2 > 5)
+
+# or we can use boolean in if else statement
+
+a = 2
+b = 28
+if a > b:
+    print("a lebih dari b")
+else:
+    print("b lebih dari a")

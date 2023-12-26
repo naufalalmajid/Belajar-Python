5527a1fd79ebbd20b8fb543a64368b4e41efa94f
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-26 17:49:55 +0700

    Update listComprehension.py

--- listComprehension.py
+++ listComprehension.py
@@ -1,1 +1,15 @@
+# by using comprehension in list, we can make new list based on an existing list using shorter syntax
+# example without comprehension
+x = ["optimist", "learning", "consistency", "humble"]
+y = []
+
+for a in x:
+    if "i" in a:
+        y.append(a)
+
+print(y)
+
+# by using comprehension, more simple (remember simple is better)
+z = [b for b in x if "i" in b]
+print(z)
 

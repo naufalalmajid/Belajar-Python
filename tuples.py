2f9e6cb6a73eab92b677f7b152590cafa1d553dc
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-30 19:50:29 +0700

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,1 +1,20 @@
+# tuples is data types, which it is ordered but unchangeable
+# for using tuples it is like list but we using round brackets
+x = ("alma", "selalu", "bersemangat", "meskipun", 2023, "hampir", "selesai")
+print(x)
 
+# tuple are ordered, unchangeable, and allowing for duplicates value
+# we can check length of tuple
+print(len(x))
+
+# tuple for one item, using commas
+y = ("learning",)
+print(type(y))
+
+# and tuple can filled with any type data like int, string, and bool
+z = ("jam", 12, "malam", "sedang", "belajar", True)
+print(type(z))
+
+# use tuple construstor, tuple()
+xyz = tuple((z))
+print(z, xyz)

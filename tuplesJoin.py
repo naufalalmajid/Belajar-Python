30a4967b00df31e73e05710d7812c9aa31beffe3
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2023-12-31 20:09:01 +0700

    Create tuplesJoin.py

---
+++ tuplesJoin.py
@@ -0,0 +1,9 @@
+# join two tuples by using + operator
+x = ("tidak ada", "badai", "yang", "tak usai")
+y = ("jadi", "tetaplah", "lakukan", "yang terbaik")
+z = x + y
+print(z)
+
+# or we can multiple tuple by using * operator
+a = x * 2
+print(a)

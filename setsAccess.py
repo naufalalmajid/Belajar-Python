5913f7fdb0d25003e088c38a2754f2c66bf8b4f6
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2024-01-01 17:53:33 +0700

    Create setsAccess.py

---
+++ setsAccess.py
@@ -0,0 +1,9 @@
+# in sets we cannot access by index value, because sets is unindexed, so we can using for loop or in keywords
+x = {"belajar", "konsisten", "mulai", "sekarang"}
+for a in x:
+    print(a)
+
+# or we can check using in if some values present in set
+print("konsisten" in x)
+
+# for remember in sets we cannt change values but we can add new items into sets

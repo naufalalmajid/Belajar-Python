34bbd628fdb37c56893f914e2908bfde874553d7
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2024-01-07 18:42:28 +0700

    Update dictAccess.py

--- dictAccess.py
+++ dictAccess.py
@@ -1,1 +1,23 @@
+# in dict we can access items by reffering to key name, inside square brackets
+myCar = {"brands": "Ford", "model": "Ranger Raptor", "year": "2022"}
+whatsmyCar = myCar["model"]
+print(whatsmyCar)
 
+# or by using get() methods will give same result
+modelmyCar = myCar.get("model")
+print(modelmyCar)
+
+# by using keys will return a list of all the keys in the dictionary
+itsmyCar = myCar.keys()
+print(itsmyCar)
+
+# make new updates in dict
+myCar["color"]: "Red"
+print(itsmyCar)
+
+favoritemyCar = myCar.values()
+print(favoritemyCar)
+
+#make new updates in dict
+myCar["engine"]: "2.0 L"
+print(favoritemyCar)

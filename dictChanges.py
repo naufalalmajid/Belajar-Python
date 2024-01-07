a8e41e465d0e69de703320c5918078e280732609
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2024-01-07 23:05:48 +0700

    Create dictChanges.py

---
+++ dictChanges.py
@@ -0,0 +1,11 @@
+# we can change dictionaries items by reffering key name
+myCar = {"brands": "Ford", "model": "Ranger Raptor", "year": "2022"}
+print(myCar)
+
+# change items
+myCar["year"] = 2023
+print(myCar)
+
+# or we can using update() method,  which it will be update items by argument (dictionary too) or an iterable object with key:values
+myCar.update({"type": "Diesel"})
+print(myCar)

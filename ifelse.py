adb65d986a0e3cfbbbd687f42662952b7f0bb8e0
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2024-01-11 23:41:11 +0700

    Update ifelse.py

--- ifelse.py
+++ ifelse.py
@@ -1,1 +1,20 @@
+# in python we have python conditions and if statements
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+
+# from conditions we can be used if statements by using if keyword, and notice the indentation for if statements
+x = 222
+y = 28
+if x > y:
+    print("x more than y")
+
+# another statements is elif which it is used for "if the previous conditions were not true, the try this condition"
+if x < y:
+    print("x less than y")
+elif x > y:
+    print("x more than y")
 

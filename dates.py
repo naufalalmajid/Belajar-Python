0ad8b191c65fe20585d0ccdd7ccedbe58e09365f
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2024-02-05 03:28:29 +0700

    Update dates.py

--- dates.py
+++ dates.py
@@ -1,1 +1,16 @@
+# python dates, we can use date object by import datetime
+import datetime
 
+x = datetime.datetime.now()
+print(x)
+
+# date output
+print(x.year)
+print(x.strftime("%j"))
+
+# creating date objects
+y = datetime.datetime(2024, 1, 1)
+print(y)
+
+# the strftime() method
+print(y.strftime("%a"))

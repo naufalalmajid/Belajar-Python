b36744ee9df80e677a4d6e7eccf34ed7117275c4
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2024-02-23 01:23:43 +0700

    Update mathematic.py

--- mathematic.py
+++ mathematic.py
@@ -1,1 +1,28 @@
+# built-in math functions, min() and max() can used for finding the lowest or highest iterable values
+x = min(2, 8, 888, 22, 222, 28, 2222, 555)
+y = max(2, 8, 888, 22, 222, 28, 2222, 555)
+print(x)
+print(y)
 
+# abs() function the absolute return
+z = abs(-28)
+print(z)
+
+# the pow(a,b), is mean value a power of b
+yxz = pow(3, 3)
+print(yxz)
+
+
+# the math module, we can using built in math module which called module
+import math
+
+xy = math.sqrt(64)
+print(int(xy))
+
+yx = math.ceil(8.8)
+yz = math.floor(8.8)
+
+print(yx, yz)
+
+phi = math.pi
+print(phi)

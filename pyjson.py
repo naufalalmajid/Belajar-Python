c46fe6fddb788e6f6d3b5368269d7f720fd08b43
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2024-02-26 19:59:36 +0700

    Update pyjson.py

--- pyjson.py
+++ pyjson.py
@@ -1,1 +1,17 @@
+# in python we have built-in package called json, which it is can be used to work with JSON data
+import json
 
+# convert from JSON to python
+x = '{ "name":"John", "age":30, "city":"New York"}'
+xpy = json.loads(x)
+
+print(type(xpy), xpy)
+
+# convert from python to JSON
+y = {"name": "almajid", "age": 21, "city": ("Singapore")}
+yjs = json.dumps(y)
+
+print(type(yjs), yjs)
+
+# format convert result, we can using indent parameter, separators, and order the result by using sort_keys parameter
+z = json.dumps(y, indent=4, sort_keys=True)

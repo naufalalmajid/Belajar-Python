3a6553629cdebbe9deac0a4ed7ae645639778216
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2024-02-29 23:01:29 +0700

    Update pyregex.py

--- pyregex.py
+++ pyregex.py
@@ -1,1 +1,11 @@
+# RegEx is Regular Exspression is sequence of characters that forms a search pattern
+# we can use RegEx module by called re built-in package in python
+import re
 
+daily = "alma always trying better than yesterday"
+find = re.search("better", daily)
+
+if find:
+    print("alma always trying to be better everyday")
+else:
+    print("alma always same with yesterday")

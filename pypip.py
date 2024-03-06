d3ba1721de161ceb362484fde35792dc2a9e27c7
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2024-03-07 04:44:08 +0700

    Create pypip.py

---
+++ pypip.py
@@ -0,0 +1,8 @@
+# pip is package manager in python, which contain module or code libraries
+print("we can check version by using pip --version in command prompt")
+print("download package by using pip install name in command prompt")
+print("using package by use import name package in code")
+print("removing pip by using pip uninstall name in command prompt")
+print("check pip installed use pip list in command prompt")
+
+# all package we can check in https://pypi.org/project/pip/

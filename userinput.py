e54de5c63abfce0cd3a649c55c49830dfbaec94c
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2024-03-09 00:32:12 +0700

    Create userinput.py

---
+++ userinput.py
@@ -0,0 +1,6 @@
+# in python we can input some value like integer or strings
+# in python 3.6 we can use input() method
+# in python 2.7 we can use raw_input()method
+
+myToyota = input("hi alma your Land Cruiser is in Garage: ")
+print(myToyota + ", go recheck again your garage alma")
